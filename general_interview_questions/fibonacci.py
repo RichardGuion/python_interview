191cb2c6efcf3bbf4a0c80a744ec7e9b76395681
Richard Guion <richardguion@gmail.com>
2022-03-29 12:01:18 -0700

    Add loop version and tests for all versions

--- general_interview_questions/fibonacci.py
+++ general_interview_questions/fibonacci.py
@@ -11,6 +11,8 @@ And the 5 is (2+3),
 and so on!
 """
 
+import pytest
+
 
 # standard way is O(2 nth power) time
 # space complexity C2C: O(n) - we have one path in memory until call stack unwinds
@@ -23,20 +25,58 @@ def fib(n):
         return fib(n - 1) + fib(n - 2)
 
 
-# Def fib_loop(n):
-# Numbers = []
-# For index in 0..n:
-# If index == 0:
-# Numbers[index] = 0
-# Elif Index == 1:
-# Numbers[index] = 1
-# Else:
-# Numbers[index] = Numbers[index-1] + Numbers[index-2]
-# Return Numbers[index-2] + Numbers[index-1]
+@pytest.mark.parametrize("number, expected", [
+    (0, 0),
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 5),
+    (6, 8),
+    (7, 13),
+    (8, 21),
+    (9, 34)
+])
+def test_fib(number, expected):
+    assert (fib(number) == expected)
+
+
+# fibonacci can also be done in a simple loop, without recursion
+# it is asked much less but once I was startled by the request to do this method,
+# after explaining both the recursion and memoization examples
+def fib_loop(n):
+    numbers = {}
+    index = 0
+    for index in range(n + 1):
+        if index < 2:
+            numbers[index] = index
+        else:
+            numbers[index] = numbers[index - 2] + numbers[index - 1]
+
+    if index > 1:
+        return numbers[index - 2] + numbers[index - 1]
+    else:
+        return index
+
+
+@pytest.mark.parametrize("number, expected", [
+    (0, 0),
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 5),
+    (6, 8),
+    (7, 13),
+    (8, 21),
+    (9, 34)
+])
+def test_fib_loop(number, expected):
+    assert (fib_loop(number) == expected)
 
 
 # uses memoization, with is O(n) time
-# space complexity C2C: O(n), because we do have it memory
+# space complexity C2C: O(n), because we do have it in memory
 # Video on memoization by Gayle Laakmann McDowell
 # https://www.youtube.com/watch?v=P8Xa2BitN3I
 def fib_memo(n, memo):
@@ -50,10 +90,25 @@ def fib_memo(n, memo):
     return memo[n]
 
 
+# Create memo data structure
 memo = {}
-z = fib_memo(9, memo)
-print(f'fib_memo(9) = {z}')
-print(f'memo = {memo}')
+
+
+@pytest.mark.parametrize("number, expected", [
+    (0, 0),
+    (9, 34),  # after this call, the memo cache should be populated and remaining values returned fast
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 5),
+    (6, 8),
+    (7, 13),
+    (8, 21),
+])
+def test_fib_memo(number, expected):
+    assert (fib_memo(number, memo) == expected)
+
 
 # this python decorator does a cache for you
 # which effectively is memoization
@@ -75,5 +130,17 @@ def fib_lru(n):
         return fib_lru(n - 1) + fib_lru(n - 2)
 
 
-z = fib_lru(9)
-print(f'fib_lru(9) = {z}')
+@pytest.mark.parametrize("number, expected", [
+    (0, 0),
+    (9, 34),  # after this call, the memo cache should be populated and remaining values returned fast
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 5),
+    (6, 8),
+    (7, 13),
+    (8, 21),
+])
+def test_fib_lru(number, expected):
+    assert (fib_lru(number) == expected)
